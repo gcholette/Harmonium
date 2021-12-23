338441d0c5febb4fcb4ce65b0b662c96b79710f9
gcholette <solutions@gcholette.com>
2021-12-22 22:22:00 -0500

    feat: rotateSteps

--- app/Main.hs
+++ app/Main.hs
@@ -6,16 +6,27 @@ module Main where
   durations
 
 -}
-data Note = A | As
+type Note = String
+
+data Step = Semitone | Tone deriving(Show)
+type Steps = [Step]
 
 type Pitch = (Note, Integer)
+
+ionianSteps :: Steps
+ionianSteps = [Tone, Tone, Semitone, Tone, Tone, Tone, Semitone]
+
+rotateSteps :: Steps -> Int -> Steps
+rotateSteps [] _ = []
+rotateSteps steps 0 = steps
+rotateSteps (x:xs) n = rotateSteps (xs ++ [x]) (n - 1)
 
 (-+-) :: [a] -> [a] -> [a]
 val1 -+- val2 = val1 ++ val2
 
 pitch1 :: Pitch
-pitch1 = (A, 3)
+pitch1 = ("A", 3)
 
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "Sup"
